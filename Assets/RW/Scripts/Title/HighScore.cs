0c72598d8e763c3844f37123357e15a4397700ef
Benjamin Mead (1000111943) <MEADBJ1@student.op.ac.nz>
2023-03-08 08:47:49 +1300

    High Score System Actually Done This Time

--- Assets/RW/Scripts/Title/HighScore.cs
+++ Assets/RW/Scripts/Title/HighScore.cs
@@ -2,20 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HighScore : MonoBehaviour
 {
+    Scene scene;
+    Text hscoreText;
     // Start is called before the first frame update
     void Start()
     {
-        Text hscoreText = this.GetComponent<Text>();
+        hscoreText = this.GetComponent<Text>();
 
         hscoreText.text = GameSettings.highScore.ToString();
+
+        scene = SceneManager.GetActiveScene();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(scene.name == "Game" && GameSettings.highScore < GameManager.Instance.sheepSaved)
+        {
+            hscoreText.text = GameManager.Instance.sheepSaved.ToString();
+        }
     }
 }
