4e339a4205655cd93e6a6414de11cb38e85d5009
Benjamin Mead (1000111943) <MEADBJ1@student.op.ac.nz>
2023-03-08 08:30:22 +1300

    Finished All Assessment Tasks, Fixed Bugs, Redoing Advanced

---
+++ Assets/RW/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+
+    [HideInInspector]
+    public int sheepSaved;
+
+    [HideInInspector]
+    public int sheepDropped;
+
+    public int sheepDroppedBeforeGameOver;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {   
+            SceneManager.LoadScene("Title");
+        }
+    }
+
+    public void SheepSaved()
+    {
+        sheepSaved++;
+        UIManager.Instance.UpdateSheepSaved();
+    }
+
+    public void SheepDropped()
+    {
+        sheepDropped++;
+        UIManager.Instance.UpdateSheepDropped();
+
+        if(sheepDropped == sheepDroppedBeforeGameOver)
+        {
+            GameOver();   
+        }
+    }
+
+    public void GameOver()
+    {
+
+        SheepSpawner.Instance.canSpawn = false;
+        SheepSpawner.Instance.DestroyAllSheep();
+
+        StartCoroutine(GameOverScreen());
+    }
+
+    IEnumerator GameOverScreen()
+    {
+        yield return new WaitForSeconds(1);
+        UIManager.Instance.ShowGameOverWindow();
+    }
+
+    public void ResetGame()
+    {
+        sheepSaved = 0;
+        sheepDropped = 0;
+        SheepSpawner.Instance.canSpawn = true;
+    }
+}
